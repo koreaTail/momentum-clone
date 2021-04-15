d5c5d866439b77b825df02d955139d4aca91a7b5
leesangjun <prokjun@gmail.com>
2021-04-15 23:37:17 +0900

    인사말바꾸기
    1. good morning afternoon evening part 수정
    
    - 다음엔 이름 입력하고 수정하게 만들자.

---
+++ greet.js
@@ -0,0 +1,15 @@
+const 인사 = 메인섹션.querySelector(".인사")
+
+console.log(new Date().getHours())
+
+if (new Date().getHours() > 21) {
+    인사.innerHTML = "Good night"
+} else if (new Date().getHours() > 17) {
+    인사.innerHTML = "Good evening"
+} else if (new Date().getHours() > 11) {
+    인사.innerHTML = "Good afternoon"
+} else if (new Date().getHours() > 4) {
+    인사.innerHTML = "Good morning"
+} else {
+    인사.innerHTML = "Good night"
+}
