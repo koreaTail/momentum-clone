6c70723afb3789683b942d5db9e003b061f60196
leesangjun <prokjun@gmail.com>
2021-04-17 00:45:03 +0900

    인사말이 안바뀌던거 수정
    인사말 체크 완료!!

--- greet.js
+++ greet.js
@@ -1,14 +1,18 @@
 const 인사말 = 메인섹션.querySelector(".인사말")
 
 
-if (new Date().getHours() > 21) {
-    인사말.innerHTML = "Good night,"
-} else if (new Date().getHours() > 17) {
-    인사말.innerHTML = "Good evening,"
-} else if (new Date().getHours() > 11) {
-    인사말.innerHTML = "Good afternoon,"
-} else if (new Date().getHours() > 4) {
-    인사말.innerHTML = "Good morning,"
-} else {
-    인사말.innerHTML = "Good night,"
+function 인사말체크() {
+    if (new Date().getHours() > 21) {
+        인사말.innerHTML = "Good night,"
+    } else if (new Date().getHours() > 17) {
+        인사말.innerHTML = "Good evening,"
+    } else if (new Date().getHours() > 11) {
+        인사말.innerHTML = "Good afternoon,"
+    } else if (new Date().getHours() > 4) {
+        인사말.innerHTML = "Good morning,"
+    } else {
+        인사말.innerHTML = "Good night,"
+    }
 }
+
+setInterval(인사말체크, 60000);
