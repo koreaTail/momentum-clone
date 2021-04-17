07ec5b2b324bdde4c01ddcf3576eebdbf4a846fa
leesangjun <prokjun@gmail.com>
2021-04-17 16:23:52 +0900

    oneThing 체크 로컬에 저장하기
    점점 꼬여간다 ㅋㅋ 내가 내껄봐도 이제 헷갈리기 시작할 것 같은.. ㅎㅎ;

--- oneThing.js
+++ oneThing.js
@@ -4,6 +4,9 @@ const 핵심일질문 = 메인섹션.querySelector(".핵심일질문")
 const 핵심일폼 = 메인섹션.querySelector(".핵심일폼")
 const 핵심일인풋 = 핵심일폼.querySelector(".핵심일인풋")
 
+const oneThingModify = oneThingDiv.querySelector(".oneThing-Modify")
+const oneThingLabel = oneThingModify.querySelector("label")
+const oneThingCheckIcon = oneThingModify.querySelector("i")
 
 // 핵심일폼에 서밋되면 -1
 // 일단 새로고침 막고 -2
@@ -27,6 +30,11 @@ function 원띵저장되어있으면할일() {
     핵심일폼.classList.add("hide");
     oneThingDiv.classList.remove("hide");
     oneThing.innerText = localStorage.getItem("oneThing")
+
+
+    if (localStorage.getItem("oneThingCheck") !== null) {
+        체크표시하기();
+    }
 }
 
 function 핵심일폼에서밋되면할일(e) {
@@ -35,27 +43,40 @@ function 핵심일폼에서밋되면할일(e) {
     원띵저장되어있으면할일();
 }
 
+function 체크표시하기() {
+    oneThingCheckIcon.classList.remove("hide")
+    oneThing.classList.add("strikethrough")
+}
 
 // 체크박스를 누르면
 // 체크모양이 보이도록
 // 다시 누르면 안보이도록
 
-const oneThingModify = oneThingDiv.querySelector(".oneThing-Modify")
-const oneThingLabel = oneThingModify.querySelector("label")
-const oneThingCheckIcon = oneThingModify.querySelector("i")
+// 완료선(취소선) 추가
 
-oneThingLabel.addEventListener("click", 체크표시만들기)
 
-function 체크표시만들기() {
+oneThingLabel.addEventListener("click", 체크표시핸들러)
 
+function 체크표시핸들러() {
     if (oneThingCheckIcon.classList.contains("hide")) {
-        oneThingCheckIcon.classList.remove("hide")
-        oneThing.classList.add("strikethrough")
+        // 체크표시하기
+        체크표시하기();
+        localStorage.setItem("oneThingCheck", true)
     } else {
+        // 체크없애기
         oneThingCheckIcon.classList.add("hide")
         oneThing.classList.remove("strikethrough")
+        localStorage.removeItem("oneThingCheck")
     }
-
 }
 
-// 완료선(취소선) 추가
+// 체크가 되어있는 상태를 저장해야해
+// 원띵이 체크되어있다고 입력하고, 불러와도 되고 '''이걸로 고고
+// 원띵 today에 같이 추가해서 forecah 로 불러와도 돼 '''쉽게가자 이건 말고
+
+// 체크 할 때 체크했다고 저장
+// 체크 취소하면 체크했다고 저장한거 삭제
+// 처음시작하면 체크했다고 저장한 기록 있으면 체크해줘
+// 없으면 말고
+
+
