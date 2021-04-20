8dddd860863e158f0990541f2f28251dc8de1493
leesangjun <prokjun@gmail.com>
2021-04-20 10:34:26 +0900

    할일박스상태 만듦
    굳! ㅋㅋ 단번에? 성공해서 살짝 기쁨 ㅎㅎ;

--- todo.js
+++ todo.js
@@ -7,4 +7,13 @@ const 할일박스 = document.querySelector(".할일박스")
 
 function 할일박스버튼작동() {
     할일박스.classList.toggle("hide")
+    if (할일박스.classList.contains("hide")) {
+        localStorage.setItem("할일박스상태", "숨겨짐")
+    } else {
+        localStorage.setItem("할일박스상태", "보여짐")
+    }
+}
+
+if (localStorage.getItem("할일박스상태") == "보여짐") {
+    할일박스버튼작동();
 }
