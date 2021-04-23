a78738ed5f6738bb5cf77fe535238aad2b6e1769
leesangjun <prokjun@gmail.com>
2021-04-23 14:08:57 +0900

    로컬에 체크 저장하는 부분에서 막힘
    ,.. 고수분의 코드를 보고오자.. ㅠ

--- todo.js
+++ todo.js
@@ -50,45 +50,48 @@ function 클래스할일입력됨추가하기() {
 }
 
 
+// 여기서 부터 고민해보자... 클래스 아이디는.. 1씩증가해야해. 중복되지 않아야해..
+
+
 function 할일입력되면할일(e) {
 
     e.preventDefault();
     클래스할일입력됨추가하기();
 
     let makeLi = document.createElement("li")
-
+    할일Ol.appendChild(makeLi);
     makeLi.innerHTML =
-        `<span class=${new Date}>
+        `<span class= "todo${할일Ol.childElementCount}">
             <div class="left">
                 <input type="checkbox">
                 <span>${todoInput.value}</span>
             </div>
             <i class="fas fa-ellipsis-h"></i>
         </span>`
-    할일Ol.appendChild(makeLi);
+
+
 
     const localValue = {
         title: todoInput.value,
         체크: false,
-        삭제: false,
-        ID: new Date
+        삭제: false
     }
 
-    localStorage.setItem(`todo-${new Date}`, JSON.stringify(localValue))
+    localStorage.setItem(`todo${할일Ol.childElementCount}`, JSON.stringify(localValue))
 
     todoInput.value = "";
 }
 
-for (let i = 1; i < 100; i++) {
-    // 이름파트 수정하기
-    if (localStorage.getItem(`이름`) !== null) {
+for (let i = 1; i < localStorage.length; i++) {
+    let todo = localStorage.getItem(`todo${i}`)
+    if (todo !== null) {
         newTodo눌림시할일();
         클래스할일입력됨추가하기();
         let makeLi = document.createElement("li")
-        makeLi.innerHTML = `<span class=${JSON.parse(localStorage.getItem(`todo-${i}`)).ID}>
+        makeLi.innerHTML = `<span class="todo${i}">
         <div class="left">
             <input type="checkbox">
-                <span>${JSON.parse(localStorage.getItem(`todo-${i}`)).title}</span>
+                <span>${JSON.parse(todo).title}</span>
             </div>
             <i class="fas fa-ellipsis-h"></i>
         </span>`
@@ -99,10 +102,26 @@ for (let i = 1; i < 100; i++) {
 할일Ol.addEventListener("change", 체크)
 
 function 체크(e) {
-    e.target.parentElement.children[1].style.color = "#7f8c8d"
-    e.target.parentElement.children[1].classList.add("strikethrough")
-    console.dir(e.target.parentElement.parentElement.className)
+
+    const targetLocalKey = e.target.parentElement.parentElement.className
+    // 체크안되어있으면, 체크 되도록
+    // 체크되어있으면, 체크 안되도록
+    if (e.target.checked) {
+        e.target.parentElement.children[1].style.color = "#7f8c8d"
+        e.target.parentElement.children[1].classList.add("strikethrough")
+
+        JSON.parse(localStorage.setItem(targetLocalKey)).체크 = "true"
+        console.log(JSON.parse(localStorage.getItem(targetLocalKey)).체크)
+    } else {
+        e.target.parentElement.children[1].style.color = "#000000"
+        e.target.parentElement.children[1].classList.remove("strikethrough")
+
+        JSON.parse(localStorage.getItem(targetLocalKey)).체크 = false
+    }
+
 }
+
+
 
 // 아이디를 넣어주지 않으면.. 구분하기가 힘들어.. ㅡㅡ;
 // 아이디를 어떻게 넣어주냐는 중요해.. 겹치지 않아야해..
